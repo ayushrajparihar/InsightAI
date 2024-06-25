b80a3c0eed5b0d5a9335d33c5b02d1cda909d4f4
Ayush <vpnxapp@gmail.com>
2024-06-26 03:57:10 +0530

    second day

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-import streamlit as 
+import streamlit as st
+import os
+from PIL import Image
+import google.generativeai as genai
+
+genai.configute(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model=genai.GenerativeModel('gemini-pro-vision')
